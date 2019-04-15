08a2a2915cf394c764e71acddf73e5840ba5a3e7
De <deniztoprak@users.noreply.github.com>
2019-04-15 09:39:10 +0200

    Fix geth-pow-docker issues (#13)
    * Fix not found npm error
    
    Fixed MaibornWolff/eden#9
    
    * Fix restarting geth service issue
    
    Fixed MaibornWolff/eden#10
    
    * Fix/Replace explorer service
    
    Fixed MaibornWolff/eden#11
    
    * Update docker-compose version number
    
    * Remove hostname and links from docker-compose
    
    * Fix/Replace netstats service
    
    Fixed MaibornWolff/eden#12
    
    * Chore use double quotation marks

--- geth-pow-docker/explorer/config.js
+++ geth-pow-docker/explorer/config.js
@@ -1,13 +1,10 @@
-'use strict'
+const web3 = require('web3');
 
-const vars = [
-    'JSONRPC_HOST',
-    'JSONRPC_PORT'
-]
-
-const config = {
-    'JSONRPC_HOST': 'localhost',
-    'JSONRPC_PORT': '8545'
+const config = function () {
+  this.logFormat = "dev";
+  this.provider = new web3.providers.HttpProvider('http://geth_mining:8545');
+  this.bootstrapUrl = "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css";
+  this.names = {};
 }
 
-module.exports = config
+module.exports = config;
