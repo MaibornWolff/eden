60635ae5e55e033430a5452f63146c456513876e
Deniz Toprak <deniztoprak@users.noreply.github.com>
2019-04-17 14:30:19 +0200

    build: add toolbox-docker in CI process

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,3 +2,4 @@
 docker-compose -f geth-poa-docker/docker-compose.yml build
 docker-compose -f geth-pow-docker/docker-compose.yml build
 docker-compose -f parity-poa-docker/docker-compose.yml build
+docker-compose -f toolbox-docker/docker-compose.yml build
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,5 @@
 docker-compose -f geth-poa-docker/docker-compose.yml push
 docker-compose -f geth-pow-docker/docker-compose.yml push
 docker-compose -f parity-poa-docker/docker-compose.yml push
+docker-compose -f toolbox-docker/docker-compose.yml push
 npx semantic-release
