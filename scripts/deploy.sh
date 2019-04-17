8bd0962048c999906878790ff1c393e18261b31f
Deniz Toprak <deniztoprak@users.noreply.github.com>
2019-04-17 09:35:33 +0200

    build: integrate semantic-releases
    Each fix && feature commit in master branch triggers a semantic
    release generating a new version number, release notes and a
    publishing package.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,3 +2,4 @@
 docker-compose -f geth-poa-docker/docker-compose.yml push
 docker-compose -f geth-pow-docker/docker-compose.yml push
 docker-compose -f parity-poa-docker/docker-compose.yml push
+npx semantic-release
