0e2a6051d5b046c03b22be4474edcbc56e0ce153
Myron Rotter <myron.rotter@maibornwolff.de>
2021-04-26 15:04:46 +0200

    refactor: use bash instead of sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-docker-compose -f geth-poa-docker/docker-compose.yml build
-docker-compose -f geth-pow-docker/docker-compose.yml build
+#!/bin/bash
+
+docker-compose -f geth-poa-docker/docker-compose.yml build --no-cache --compress --progress plain --memory 6g
+docker-compose -f geth-pow-docker/docker-compose.yml build --no-cache --compress --progress plain
 docker-compose -f parity-poa-docker/docker-compose.yml build
-docker-compose -f toolbox-docker/docker-compose.yml build
+docker-compose -f toolbox-docker/docker-compose.yml build --no-cache --compress --progress plain
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
 docker-compose -f geth-poa-docker/docker-compose.yml push
 docker-compose -f geth-pow-docker/docker-compose.yml push
 docker-compose -f parity-poa-docker/docker-compose.yml push
 docker-compose -f toolbox-docker/docker-compose.yml push
+
 npx semantic-release
