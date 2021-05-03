f040cfd25bb349d100aad6fccc9014c44bf07459
Myron Rotter <myron.rotter@maibornwolff.de>
2021-05-03 11:03:46 +0200

    fix: add missing setups to build and deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,8 @@
 
 docker-compose -f geth-poa-docker/docker-compose.yml build --no-cache --compress --progress plain --memory 6g
 docker-compose -f geth-pow-docker/docker-compose.yml build --no-cache --compress --progress plain
-docker-compose -f parity-poa-docker/docker-compose.yml build
+docker-compose -f ipfs-docker/docker-compose.yml build --no-cache --compress --progress plain
+docker-compose -f openethereum-docker/docker-compose.yml build --no-cache --compress --progress plain
+docker-compose -f parity-poa-docker/docker-compose.yml build --no-cache --compress --progress plain
+docker-compose -f skale-docker/docker-compose.yml build --no-cache --compress --progress plain
 docker-compose -f toolbox-docker/docker-compose.yml build --no-cache --compress --progress plain
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,10 @@
 
 docker-compose -f geth-poa-docker/docker-compose.yml push
 docker-compose -f geth-pow-docker/docker-compose.yml push
+docker-compose -f ipfs-docker/docker-compose.yml push
+docker-compose -f openethereum-docker/docker-compose.yml push
 docker-compose -f parity-poa-docker/docker-compose.yml push
+docker-compose -f skale-docker/docker-compose.yml push
 docker-compose -f toolbox-docker/docker-compose.yml push
 
 npx semantic-release
